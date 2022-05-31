564c713a29d3b32656ba5010fd31b720c1cae066
DavidKwon7 <khjunejune10@gmail.com>
2022-05-31 15:17:50 +0900

    NetworkModule 작성

---
+++ app/src/main/java/com/example/android_clean_architecture/di/NetworkModule.kt
@@ -0,0 +1,56 @@
+package com.example.android_clean_architecture.di
+
+import androidx.viewbinding.BuildConfig
+import com.example.remote.api.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG)
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        else
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.NONE
+        return httpLoggingInterceptor
+    }
+
+    @Provides
+    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor) : OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .addInterceptor(httpLoggingInterceptor)
+            .retryOnConnectionFailure(true)
+            .build()
+    }
+
+    @Provides
+    fun provideRetrofitTest(okHttpClient: OkHttpClient) : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://jsonplaceholder.typicode.com/")
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+}
