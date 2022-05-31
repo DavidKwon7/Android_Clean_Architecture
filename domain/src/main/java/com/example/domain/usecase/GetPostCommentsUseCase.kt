7f8f2ae1f40547d632b7c45f1691ca0fb1e7c3a0
DavidKwon7 <khjunejune10@gmail.com>
2022-05-31 17:29:03 +0900

    GetPostUseCase 작성

--- domain/src/main/java/com/example/domain/usecase/GetPostCommentsUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/GetPostCommentsUseCase.kt
@@ -1,9 +1,28 @@
 package com.example.domain.usecase
 
+import com.example.common.extensions.Resource
+import com.example.domain.entity.CommentEntityModel
+import com.example.domain.qualifiers.IoDispatcher
 import com.example.domain.repository.Repository
+import com.example.domain.usecase.core.BaseUseCase
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.flowOn
+import java.lang.Exception
 import javax.inject.Inject
 
 class GetPostCommentsUseCase @Inject constructor(
-    private val repository: Repository
-) {
+    private val repository: Repository,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher
+) : BaseUseCase<List<CommentEntityModel>, Int>() {
+    override suspend fun buildRequest(params: Int?): Flow<Resource<List<CommentEntityModel>>> {
+        if (params == null) {
+            return flow {
+                emit(Resource.Error(Exception("PostId can not be null")))
+            }.flowOn(dispatcher)
+        }
+        return repository.getPostComments(postId = params).flowOn(dispatcher)
+    }
 }
