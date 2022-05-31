57090cecad6fd820a076ef924d6adcc11f75a438
DavidKwon7 <khjunejune10@gmail.com>
2022-05-31 15:55:43 +0900

    mapperModule : remote 부분 작성

--- app/src/main/java/com/example/android_clean_architecture/di/MapperModule.kt
+++ app/src/main/java/com/example/android_clean_architecture/di/MapperModule.kt
@@ -1,5 +1,17 @@
 package com.example.android_clean_architecture.di
 
+import com.example.common.Mapper
+import com.example.data.mapper.CommentDataDomainMapper
+import com.example.data.mapper.PostDataDomainMapper
+import com.example.data.model.CommentDataModel
+import com.example.data.model.PostDataModel
+import com.example.domain.entity.CommentEntityModel
+import com.example.domain.entity.PostEntityModel
+import com.example.remote.mapper.CommentNetworkDataMapper
+import com.example.remote.mapper.PostNetworkDataMapper
+import com.example.remote.model.CommentNetworkModel
+import com.example.remote.model.PostNetworkModel
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -7,4 +19,13 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class MapperModule {
+
+    // remote
+    @Binds
+    abstract fun bindPostNetworkDataMapper(mapper: PostNetworkDataMapper): Mapper<PostNetworkModel, PostDataModel>
+    @Binds
+    abstract fun bindCommentNetworkDataMapper(mapper: CommentNetworkDataMapper): Mapper<CommentNetworkModel, CommentDataModel>
+
+
+
 }
