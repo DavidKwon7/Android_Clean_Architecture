72ff15740cbc13f8befc6a260267bca8cae6f0ce
DavidKwon7 <khjunejune10@gmail.com>
2022-06-01 02:53:08 +0900

    CommentDomainUiMapper 작성

--- presentation/src/main/java/com/example/presentation/mapper/CommentDomainUiMapper.kt
+++ presentation/src/main/java/com/example/presentation/mapper/CommentDomainUiMapper.kt
@@ -1,7 +1,28 @@
 package com.example.presentation.mapper
 
 import com.example.common.Mapper
+import com.example.domain.entity.CommentEntityModel
+import com.example.presentation.model.CommentUiModel
 import javax.inject.Inject
 
-class CommentDomainUiMapper @Inject constructor() {
+class CommentDomainUiMapper @Inject constructor(): Mapper<CommentUiModel, CommentEntityModel> {
+    override fun from(i: CommentUiModel?): CommentEntityModel {
+        return CommentEntityModel(
+            postId = i?.postId,
+            id = i?.id,
+            name = i?.name,
+            email = i?.email,
+            body = i?.body
+        )
+    }
+
+    override fun to(o: CommentEntityModel?): CommentUiModel {
+        return CommentUiModel(
+            postId = o?.postId,
+            id = o?.id,
+            name = o?.name,
+            email = o?.email,
+            body = o?.body
+        )
+    }
 }
