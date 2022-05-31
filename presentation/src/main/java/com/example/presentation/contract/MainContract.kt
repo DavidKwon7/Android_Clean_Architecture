c85fe4357c43b0fafa265546be1140b555675ac5
DavidKwon7 <khjunejune10@gmail.com>
2022-06-01 03:33:33 +0900

    MainContract 수정

--- presentation/src/main/java/com/example/presentation/contract/MainContract.kt
+++ presentation/src/main/java/com/example/presentation/contract/MainContract.kt
@@ -4,25 +4,26 @@ import com.example.base.UiEffect
 import com.example.base.UiEvent
 import com.example.presentation.model.PostUiModel
 
+
 class MainContract {
 
-    sealed class Event: UiEvent {
+    sealed class Event : UiEvent {
         object OnFetchPosts: Event()
-        data class OnPostItemClicked(val post: PostUiModel): Event()
+        data class OnPostItemClicked(val post: PostUiModel) : Event()
     }
 
-    data class State (
-        val postState: PostsState,
+    data class State(
+        val postsState: PostsState,
         val selectedPost: PostUiModel? = null
-            )
+    )
 
     sealed class PostsState {
         object Idle: PostsState()
         object Loading: PostsState()
-        data class Success(val posts: List<PostUiModel>): PostsState()
+        data class Success(val posts: List<PostUiModel>) : PostsState()
     }
 
-    sealed class Effect: UiEffect {
-        data class showError(val message: String?): Effect()
+    sealed class Effect : UiEffect {
+        data class ShowError(val message: String?) : Effect()
     }
 }
