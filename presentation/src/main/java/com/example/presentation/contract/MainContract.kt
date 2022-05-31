4ece928fb797268dcbed16e0c9122fc8d7fe1896
DavidKwon7 <khjunejune10@gmail.com>
2022-06-01 03:26:29 +0900

    MainContract 작성

--- presentation/src/main/java/com/example/presentation/contract/MainContract.kt
+++ presentation/src/main/java/com/example/presentation/contract/MainContract.kt
@@ -1,4 +1,28 @@
 package com.example.presentation.contract
 
+import com.example.base.UiEffect
+import com.example.base.UiEvent
+import com.example.presentation.model.PostUiModel
+
 class MainContract {
+
+    sealed class Event: UiEvent {
+        object OnFetchPosts: Event()
+        data class OnPostItemClicked(val post: PostUiModel): Event()
+    }
+
+    data class State (
+        val postState: PostsState,
+        val selectedPost: PostUiModel? = null
+            )
+
+    sealed class PostsState {
+        object Idle: PostsState()
+        object Loading: PostsState()
+        data class Success(val posts: List<PostUiModel>): PostsState()
+    }
+
+    sealed class Effect: UiEffect {
+        data class showError(val message: String?): Effect()
+    }
 }
