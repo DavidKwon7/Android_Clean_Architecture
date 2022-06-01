8e357ffc4fbafc4af909fc0a95f6d13083cd637f
DavidKwon7 <khjunejune10@gmail.com>
2022-06-01 13:44:22 +0900

    data module : MockK 추가 (build.gradle)

---
+++ data/src/test/java/com/example/data/RepositoryImpTest.kt
@@ -0,0 +1,11 @@
+package com.example.data
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlin.time.ExperimentalTime
+
+@ExperimentalTime
+@ExperimentalCoroutinesApi
+class RepositoryImpTest {
+
+
+}
