9f854f21915749b844a25b5b80e38dda3f981c86
DavidKwon7 <khjunejune10@gmail.com>
2022-06-01 10:31:33 +0900

    Main vm 수정

--- presentation/src/main/java/com/example/presentation/vm/MainViewModel.kt
+++ presentation/src/main/java/com/example/presentation/vm/MainViewModel.kt
@@ -1,4 +1,13 @@
 package com.example.presentation.vm
 
-class MainViewModel {
+import com.example.common.Mapper
+import com.example.domain.entity.CommentEntityModel
+import com.example.domain.usecase.GetPostsUseCase
+import com.example.presentation.model.CommentUiModel
+import javax.inject.Inject
+
+class MainViewModel @Inject constructor(
+    private val getPostsUseCase: GetPostsUseCase,
+    private val commentMapper: Mapper<CommentEntityModel, CommentUiModel>
+)  {
 }
