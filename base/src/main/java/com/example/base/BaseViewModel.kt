c12cc5af5af14983bc85539d40bd4608f012c652
DavidKwon7 <khjunejune10@gmail.com>
2022-06-01 12:28:00 +0900

    base vm

--- base/src/main/java/com/example/base/BaseViewModel.kt
+++ base/src/main/java/com/example/base/BaseViewModel.kt
@@ -1,11 +1,9 @@
 package com.example.base
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-abstract class BaseViewModel<Event: UiEvent, State: UiState, Effect: UiEffect> : ViewModel() {
-
-    private val initialState : State by lazy { createInitialState() }
-    abstract fun createInitialState(): State
-
+abstract class BaseViewModel<Event: UiEvent, State: UiState, Effect: UiEffect>
+    : ViewModel() {
 
 }
