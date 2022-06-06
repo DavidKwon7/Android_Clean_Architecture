0a649d9613a99f50540acdf2f1b3333e25c0cd33
DavidKwon7 <khjunejune10@gmail.com>
2022-06-06 15:03:42 +0900

    vm : basic setting

--- presentation/src/main/java/com/example/presentation/vm/DetailViewModel.kt
+++ presentation/src/main/java/com/example/presentation/vm/DetailViewModel.kt
@@ -1,5 +1,12 @@
 package com.example.presentation.vm
 
-class DetailViewModel {
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailViewModel @Inject constructor(
+
+): ViewModel() {
 
 }
--- presentation/src/main/java/com/example/presentation/vm/MainViewModel.kt
+++ presentation/src/main/java/com/example/presentation/vm/MainViewModel.kt
@@ -1,13 +1,16 @@
 package com.example.presentation.vm
 
+import androidx.lifecycle.ViewModel
 import com.example.common.Mapper
 import com.example.domain.entity.CommentEntityModel
 import com.example.domain.usecase.GetPostsUseCase
 import com.example.presentation.model.CommentUiModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class MainViewModel @Inject constructor(
     private val getPostsUseCase: GetPostsUseCase,
     private val commentMapper: Mapper<CommentEntityModel, CommentUiModel>
-)  {
+): ViewModel() {
 }
