26bc59ae7b33ee79a78c794c71e61deb2ab3a096
DavidKwon7 <khjunejune10@gmail.com>
2022-06-06 15:01:34 +0900

    presentation module : test data 작성

---
+++ presentation/src/test/java/com/example/presentation/TestDataGenerator.kt
@@ -0,0 +1,22 @@
+package com.example.presentation
+
+import com.example.presentation.model.CommentUiModel
+import com.example.presentation.model.PostUiModel
+
+class TestDataGenerator {
+    companion object {
+        fun generatePosts(): List<PostUiModel> {
+            val item1 = PostUiModel(1, 1, "title 1", "test body 1")
+            val item2 = PostUiModel(1, 2, "title 2", "test body 2")
+            val item3 = PostUiModel(1, 3, "title 3", "test body 3")
+            return listOf(item1, item2, item3)
+        }
+
+        fun generatePostComments(): List<CommentUiModel> {
+            val item1 = CommentUiModel(1,1, "test name 1", "test mail 1", "test body 1")
+            val item2 = CommentUiModel(1,2, "test name 2", "test mail 2", "test body 2")
+            val item3 = CommentUiModel(1,3, "test name 3", "test mail 3", "test body 3")
+            return listOf(item1, item2, item3)
+        }
+    }
+}
