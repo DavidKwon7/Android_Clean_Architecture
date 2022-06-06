91eab25b06f4b84745afea463c2bdcbf08fb4110
DavidKwon7 <khjunejune10@gmail.com>
2022-06-06 14:08:27 +0900

    MainActivity.kt 내용 수정 (삭제)

--- app/src/main/java/com/example/android_clean_architecture/MainActivity.kt
+++ app/src/main/java/com/example/android_clean_architecture/MainActivity.kt
@@ -7,31 +7,9 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    var count = 0
-    private lateinit var tv : TextView
-    private lateinit var btn: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tv = findViewById<TextView>(R.id.tv)
-        val btn = findViewById<Button>(R.id.btn)
-
-        btn.setOnClickListener {
-            count += 1
-            tv.setText(count)
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt("count", count)
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        count = savedInstanceState.getInt("count",0)
-        tv.setText(count)
     }
 }
