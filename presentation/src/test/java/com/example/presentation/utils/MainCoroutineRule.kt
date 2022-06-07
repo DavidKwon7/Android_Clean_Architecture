504462ef89d68e323bd11ae4744fbc4e385b3c4c
DavidKwon7 <khjunejune10@gmail.com>
2022-06-07 15:23:09 +0900

    Test: MainCoroutineRule.kt 생성, dependency 선언

---
+++ presentation/src/test/java/com/example/presentation/utils/MainCoroutineRule.kt
@@ -0,0 +1,8 @@
+package com.example.presentation.utils
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
+@ExperimentalCoroutinesApi
+class MainCoroutineRule {
+}
