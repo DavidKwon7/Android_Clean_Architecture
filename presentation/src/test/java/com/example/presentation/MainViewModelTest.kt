fabf8891dfd8cd435336e6025f3210caa3f9a4f5
DavidKwon7 <khjunejune10@gmail.com>
2022-06-07 15:59:56 +0900

    presentation : dependency 추가

---
+++ presentation/src/test/java/com/example/presentation/MainViewModelTest.kt
@@ -0,0 +1,12 @@
+package com.example.presentation
+
+import androidx.test.filters.SmallTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlin.time.ExperimentalTime
+
+@ExperimentalTime
+@ExperimentalCoroutinesApi
+@SmallTest
+class MainViewModelTest {
+
+}
