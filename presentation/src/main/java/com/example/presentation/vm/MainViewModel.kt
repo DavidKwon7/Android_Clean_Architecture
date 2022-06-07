f0bef846fc5a6ecd55f374b2f5251e6748551f2d
DavidKwon7 <khjunejune10@gmail.com>
2022-06-07 10:49:56 +0900

    BaseViewModel.kt 작성

--- presentation/src/main/java/com/example/presentation/vm/MainViewModel.kt
+++ presentation/src/main/java/com/example/presentation/vm/MainViewModel.kt
@@ -3,14 +3,16 @@ package com.example.presentation.vm
 import androidx.lifecycle.ViewModel
 import com.example.common.Mapper
 import com.example.domain.entity.CommentEntityModel
+import com.example.domain.entity.PostEntityModel
 import com.example.domain.usecase.GetPostsUseCase
 import com.example.presentation.model.CommentUiModel
+import com.example.presentation.model.PostUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val getPostsUseCase: GetPostsUseCase,
-    private val commentMapper: Mapper<CommentEntityModel, CommentUiModel>
+    private val postsUseCase: GetPostsUseCase,
+    private val postMapper: Mapper<PostEntityModel, PostUiModel>
 ): ViewModel() {
 }
