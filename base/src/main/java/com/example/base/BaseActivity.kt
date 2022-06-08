ada4c2b2c4bee1e8d6c8281c658b381ebfc22a94
DavidKwon7 <khjunejune10@gmail.com>
2022-06-08 23:46:20 +0900

    BaseActivity.kt 작성

--- base/src/main/java/com/example/base/BaseActivity.kt
+++ base/src/main/java/com/example/base/BaseActivity.kt
@@ -5,5 +5,10 @@ import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseActivity<VB: ViewBinding>{
-}
+/**
+ * [AppCompatActivity] 여기에서 계속 에러가 생긴다..
+ * multimodule - ui 관련해서 문제가 발생한거 같은데... 흠..
+ * 우선 app module 에 생성하기로 하였음.. ㅠ
+ */
+
+// abstract class BaseActivity<VB: ViewBinding> : AppCompatActivity(){ }
