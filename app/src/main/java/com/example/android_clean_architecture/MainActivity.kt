86e50af9a7310b99b3d3f3d06d4d9fbda4e44a01
DavidKwon7 <khjunejune10@gmail.com>
2022-06-08 23:48:00 +0900

    MainActivity.kt setting

--- app/src/main/java/com/example/android_clean_architecture/MainActivity.kt
+++ app/src/main/java/com/example/android_clean_architecture/MainActivity.kt
@@ -2,14 +2,17 @@ package com.example.android_clean_architecture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.TextView
+import com.example.android_clean_architecture.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override val bindLayout: (LayoutInflater) -> ActivityMainBinding
+        get() = ActivityMainBinding::inflate
 
+    override fun prepareView(savedInstanceState: Bundle?) {
+        // -a-
     }
 }
