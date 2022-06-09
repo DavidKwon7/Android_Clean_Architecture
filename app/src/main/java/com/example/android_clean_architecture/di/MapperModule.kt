96820d81e749a061cb3b43014346ea89e290692b
DavidKwon7 <khjunejune10@gmail.com>
2022-06-09 22:57:43 +0900

    di: presentation mapper 추가

--- app/src/main/java/com/example/android_clean_architecture/di/MapperModule.kt
+++ app/src/main/java/com/example/android_clean_architecture/di/MapperModule.kt
@@ -7,6 +7,10 @@ import com.example.data.model.CommentDataModel
 import com.example.data.model.PostDataModel
 import com.example.domain.entity.CommentEntityModel
 import com.example.domain.entity.PostEntityModel
+import com.example.presentation.mapper.CommentDomainUiMapper
+import com.example.presentation.mapper.PostDomainUiMapper
+import com.example.presentation.model.CommentUiModel
+import com.example.presentation.model.PostUiModel
 import com.example.remote.mapper.CommentNetworkDataMapper
 import com.example.remote.mapper.PostNetworkDataMapper
 import com.example.remote.model.CommentNetworkModel
@@ -37,4 +41,12 @@ abstract class MapperModule {
     @Binds
     abstract fun bindCommentDataMapper(mapper: CommentDataDomainMapper): Mapper<CommentDataModel, CommentEntityModel>
 
+    /**
+     * [presentation] mapper
+     */
+    @Binds
+    abstract fun bindPostDomainUiMapper(mapper: PostDomainUiMapper): Mapper<PostEntityModel, PostUiModel>
+
+    @Binds
+    abstract fun bindCommentDomainUiMapper(mapper: CommentDomainUiMapper): Mapper<CommentEntityModel, CommentUiModel>
 }
