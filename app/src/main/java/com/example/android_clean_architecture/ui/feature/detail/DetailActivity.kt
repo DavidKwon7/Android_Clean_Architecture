3c6bf430f70ea23e8696fa69363724df3a33e53d
DavidKwon7 <khjunejune10@gmail.com>
2022-06-12 23:20:26 +0900

    detail activity 생성

---
+++ app/src/main/java/com/example/android_clean_architecture/ui/feature/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android_clean_architecture.ui.feature.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android_clean_architecture.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
